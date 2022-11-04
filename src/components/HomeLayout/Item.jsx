57c0b2e87f1f0947dbf38c85c030d33734878f07
tuan67720@gmail.com <tuan67720@tuan67720gmail.67720gmailcom>
2022-11-04 17:07:52 +0700

    'first'

--- src/components/HomeLayout/Item.jsx
+++ src/components/HomeLayout/Item.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import style from '../HomeLayout/DemoModule/Item.module.css'
 export default class
     extends Component {
     render() {
@@ -9,9 +9,9 @@ export default class
                     <div className="content p-5">
                         <div className="list row">
                             {/*  */}
-                            <div className="main p-3 rounded-3 text-center col-4">
+                            <div  className="main p-3 rounded-3 text-center col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
                                 <div className="content p-5 bg-light ">
-                                <div className="bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3" >
+                                <div className={`${style['main']} bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3`}>
                                 <i class="fa fa-download"></i>
                                 </div>
                                 <h3>Fresh new layout</h3>
@@ -19,45 +19,47 @@ export default class
                                 </div>
                             </div>
                             {/*  */}
-                            <div className="main p-3 rounded-3 text-center col-4">
+                            <div  className="main p-3 rounded-3 text-center col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
                                 <div className="content p-5 bg-light ">
-                                <div className="bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3" >
+                                <div className={`${style['main']} bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3`}>
                                 <i class="fa fa-download"></i>
                                 </div>
                                 <h3>Fresh new layout</h3>
                                 <p>With Bootstrap 5, we've created a fresh new layout for this template!</p>
                                 </div>
                             </div>
-                            <div className="main p-3 rounded-3 text-center col-4">
+                            <div  className="main p-3 rounded-3 text-center col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
                                 <div className="content p-5 bg-light ">
-                                <div className="bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3" >
+                                <div className={`${style['main']} bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3`}>
+                                <i class="fa fa-download"></i>
+                                </div>
+                                <h3>Fresh new layout</h3>
+                                <p style={{
+                                    color: "red",
+                                }}>With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                                </div>
+                            </div>
+                            <div  className="main p-3 rounded-3 text-center col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                                <div className="content p-5 bg-light ">
+                                <div className={`${style['main']} bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3`}>
                                 <i class="fa fa-download"></i>
                                 </div>
                                 <h3>Fresh new layout</h3>
                                 <p>With Bootstrap 5, we've created a fresh new layout for this template!</p>
                                 </div>
                             </div>
-                            <div className="main p-3 rounded-3 text-center col-4">
+                            <div  className="main p-3 rounded-3 text-center col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
                                 <div className="content p-5 bg-light ">
-                                <div className="bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3" >
+                                <div className={`${style['main']} bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3`}>
                                 <i class="fa fa-download"></i>
                                 </div>
                                 <h3>Fresh new layout</h3>
                                 <p>With Bootstrap 5, we've created a fresh new layout for this template!</p>
                                 </div>
                             </div>
-                            <div className="main p-3 rounded-3 text-center col-4">
+                            <div  className="main p-3 rounded-3 text-center col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
                                 <div className="content p-5 bg-light ">
-                                <div className="bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3" >
-                                <i class="fa fa-download"></i>
-                                </div>
-                                <h3>Fresh new layout</h3>
-                                <p>With Bootstrap 5, we've created a fresh new layout for this template!</p>
-                                </div>
-                            </div>
-                            <div className="main p-3 rounded-3 text-center col-4">
-                                <div className="content p-5 bg-light ">
-                                <div className="bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3" >
+                                <div className={`${style['main']} bg-primary bg-gradient text-white rounded-3 p-3 m-auto w-20 mb-3`}>
                                 <i class="fa fa-download"></i>
                                 </div>
                                 <h3>Fresh new layout</h3>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HomeLayout from './components/HomeLayout/HomeLayout';
-// import './index.scss'
+import './index.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
